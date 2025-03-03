9ba572ced69a3b417395406d41238fe3d699a439
Xu <zix039@ieng6-203.ucsd.edu>
2025-03-03 11:09:02 -0800

    stack_pu

--- stack.c
+++ stack.c
@@ -10,4 +10,28 @@ void clear() {
         pop();
 }
 
+void push(int num) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = num;
+    newNode->next = stackTop;
+    stackTop = newNode;
+}
+
+int pop() {
+    int data = stackTop->data;
+    struct Node *temp = stackTop;
+    stackTop = stackTop->next;
+    free(temp);
+    return data;
+}
+
+void copy_to_array(int *array, size_t space) {
+    struct Node *current = stackTop;
+    size_t i = 0;
+    while (current != NULL && i < space) {
+        array[i++] = current->data;
+        current = current->next;
+    }
+}
+
 // Implement your team's function here!
